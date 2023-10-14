57e1141fe637a245ec06ced8cc431e9e04e10212
Ram Jondhale <ramjondhale1@gmail.com>
2023-10-14 13:19:45 +0530

    Initial Commit

---
+++ H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by Ram Jondhale on 14/10/23.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
